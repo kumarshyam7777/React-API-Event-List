1a3cd5025526bed173eb6b7fd09c1e7e7da90e17
kumarshyam7777 <kumar.shyam7777@gmail.com>
2021-12-31 14:31:27 +0530

    Initial Commit

--- src/component/ImageCard.js
+++ src/component/ImageCard.js
@@ -3,12 +3,23 @@ import React, { Component } from "react";
 class ImageCard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      spans: 0,
+    };
     this.imageRef = React.createRef();
   }
 
   componentDidMount() {
     console.log(this.imageRef);
+    this.imageRef.current.addEventListener("load", this.setSpans);
   }
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  };
   render() {
     const { description, urls } = this.props.image;
     return (
